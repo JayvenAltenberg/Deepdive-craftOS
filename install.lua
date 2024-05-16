6cb143a172725c79a266075a990e1e4a62642c14
JayvenAltenberg <151913729+JayvenAltenberg@users.noreply.github.com>
2024-05-16 14:26:25 +0200

    Add files via upload

---
+++ install.lua
@@ -0,0 +1,18 @@
+wget run
+response = http.get("https://raw.githubusercontent.com/JayvenAltenberg/Deepdive-craftOS/main/chest_monitor.lua")
+
+if response then
+    local content = response.readAll()
+    response.close()
+    local file = fs.open("chest_monitor.lua", "w")
+    file.write(content)
+    file.close()
+
+    local startupFile = fs.open("startup.lua", "w")
+    startupFile.write("shell.run('chest_monitor.lua')")
+    startupFile.close()
+
+    shell.run("reboot")
+else
+    print("")
+end
